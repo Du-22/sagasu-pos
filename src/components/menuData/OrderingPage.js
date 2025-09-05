fac8cc9e838881a534d93e83072d63b7d6afe00b
Du <du88215@gmail.com>
2025-09-05 19:48:03 +0800

    新增入座狀態下的釋放桌子按鈕

--- src/components/menuData/OrderingPage.js
+++ src/components/menuData/OrderingPage.js
@@ -6,7 +6,7 @@ import OrderSummary from "../UI/OrderSummary";
 const OrderingPage = ({
   selectedTable,
   currentOrder,
-  confirmedOrdersBatches, // 新增
+  confirmedOrdersBatches,
   tableStatus,
   onBack,
   onAddToOrder,
@@ -14,9 +14,10 @@ const OrderingPage = ({
   onRemoveItem,
   onSubmitOrder,
   onCheckout,
-  onEditConfirmedItem, // 新增
+  onEditConfirmedItem,
   onMenuSelect,
   menuData,
+  onReleaseSeat,
 }) => (
   <div className="min-h-screen bg-gray-100">
     <Header
@@ -33,16 +34,27 @@ const OrderingPage = ({
         <MenuArea menuData={menuData} onAddToOrder={onAddToOrder} />
       </div>
       <div className="w-1/3 h-full flex flex-col">
+        {/* 入座狀態才顯示釋放桌子按鈕 */}
+
+        {tableStatus === "seated" && (
+          <button
+            onClick={() => onReleaseSeat(selectedTable)}
+            className="bg-gray-700 hover:bg-gray-800 text-white px-4 py-2 rounded mt-2 transition"
+          >
+            釋放桌子
+          </button>
+        )}
+
         <OrderSummary
           currentOrder={currentOrder}
-          confirmedOrdersBatches={confirmedOrdersBatches} // 新增
+          confirmedOrdersBatches={confirmedOrdersBatches}
           selectedTable={selectedTable}
           tableStatus={tableStatus}
           onUpdateQuantity={onUpdateQuantity}
           onRemoveItem={onRemoveItem}
           onSubmitOrder={onSubmitOrder}
           onCheckout={onCheckout}
-          onEditConfirmedItem={onEditConfirmedItem} // 新增
+          onEditConfirmedItem={onEditConfirmedItem}
         />
       </div>
     </div>
